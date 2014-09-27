00587b22fd25c95f83a5b2246e465dd1792f0985
rygim <rygimmy@gmail.com>
2014-09-27 18:14:58 -0400

    editted application controller, devise edit view, devise new view, removed links from header, configured omniauth routes and added the users omniauth controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
-
-
+  devise_for :users, controllers: {omniauth_callbacks: 'users/omniauth_callbacks'}
   resources :posts
 
   get 'pages/home'
